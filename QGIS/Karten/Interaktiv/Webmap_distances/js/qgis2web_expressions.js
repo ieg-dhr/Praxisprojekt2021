59ab9dcbd4dee9a664251e46073b1bbec6d93735
mmiche01 <mmiche01@students.uni-mainz.de>
2022-01-02 12:49:31 +0100

    Improve distances map

--- QGIS/Karten/Interaktiv/Webmap_distances/js/qgis2web_expressions.js
+++ QGIS/Karten/Interaktiv/Webmap_distances/js/qgis2web_expressions.js
@@ -754,17 +754,3 @@ function fnc__specialcol_(values, context) {
 function fnc_project_color(values, context) {
     return false;
 };
-
-
-
-function exp_Paths_1rule0_eval_expression(context) {
-    // Document_type = 'Geburtsbrief' OR Document_type = 'Geburtsurkunde'
-
-    var feature = context.feature;
-    
-    if (feature.properties) {
-        return ((feature.properties['Document_type']  == 'Geburtsbrief') || (feature.properties['Document_type']  == 'Geburtsurkunde'));
-    } else {
-        return ((feature['Document_type']  == 'Geburtsbrief') || (feature['Document_type']  == 'Geburtsurkunde'));
-    }
-}
